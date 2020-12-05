20d1608901c7a39c1dca5d0e79773203795bec93
David Gubitosi <david.gubitosi@gmail.com>
2020-12-04 22:21:09 -0500

    day 10

---
+++ day10/part2.py
@@ -0,0 +1,22 @@
+
+import sys
+
+try:
+    n = sys.argv[1]
+except:
+    n = str(3113322113)
+
+for i in range(50):
+    prev = n
+    n = ''
+    j = 0
+    while j < len(prev):
+        char = prev[j]
+        k = j + 1
+        while k < len(prev) and prev[k] == prev[j]:
+            k += 1
+        count = k - j
+        n += str(count) + char
+        j += count
+    #print(i, prev, n)
+print(len(n))
