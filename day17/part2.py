c0a61c179cc3e12559cb20d0ec84b55264394426
David Gubitosi <david.gubitosi@gmail.com>
2020-12-08 17:56:28 -0500

    day 17, part2

--- day17/part2.py
+++ day17/part2.py
@@ -2,9 +2,8 @@
 # https://stackoverflow.com/questions/4632322/finding-all-possible-combinations-of-numbers-to-reach-a-given-sum
 def subset_sum(numbers, target, partial=[], results=[]):
     s = sum(partial)
-    if s == target:
-        #print("sum{}={}".format(tuple(partial), target))
-        results.append(tuple(partial))
+    if s == target: 
+        results.append(len(partial))
         return
     for i in range(len(numbers)):
         n = numbers[i]
@@ -12,8 +11,15 @@ def subset_sum(numbers, target, partial=[], results=[]):
         subset_sum(remaining, target, partial + [n], results)
     return results
 
-containers = list()
+containers = list() 
 with open("input.txt") as f:
     containers = [ int(x.strip()) for x in f.readlines() ]
-print(len(subset_sum(containers, 150)))
+
+d = dict()
+for s in subset_sum(containers, 150):
+    d.setdefault(s, 0)
+    d[s]+=1
+for x in sorted(d):
+    print(x, d[x])
+
 
