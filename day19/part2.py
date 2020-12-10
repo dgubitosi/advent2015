cfc062014ce1c9e8dc33930567b984db7b8777ae
David Gubitosi <david.gubitosi@gmail.com>
2020-12-09 20:42:10 -0500

    day 19, part 2, ugly

--- day19/part2.py
+++ day19/part2.py
@@ -19,13 +19,37 @@ print(target)
 print()
 
 # work backwards from target, reducing the instances of {foo}Rn{bar}Ar ??
+# some the {bar} pieces need to be reduced
 
 import re
-for iter in re.finditer(r'Rn[^Rn]+?Ar', target):
-    start = iter.start() - 1
-    if target[start].islower(): start -= 1
-    end = iter.end()
-    print(start,end,target[start:end])
+
+matches = re.findall(r'Rn[^Rn]+?Ar', target)
+PREV = set()
+while matches:
+    if PREV == set(matches): break
+    print("MATCHES:", matches)
+    for x in matches:
+        print("MATCH:",x)
+        m = re.search(x, target)
+        start = m.start() - 1
+        if target[start].islower(): start -= 1
+        end = m.end()
+        substr = target[start:end]
+        print(start,end,substr)
+        found = False
+        for r in replacements:
+            for c in replacements[r]:
+                if c == substr:
+                    found = True
+                    break
+            if found:
+                print(start,end,substr,r)
+                target = target[0:start] + r + target[end:]
+                print(target)
+                break
+    PREV = set(matches)
+    matches = re.findall(r'Rn[^Rn]+?Ar', target)
+print(target)
 
 '''
 # lol i didn't think brute force would work
